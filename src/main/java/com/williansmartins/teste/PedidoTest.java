f46d3485343eec178f0979796daeae2cb85d25a2
williansmartins <professorwillians@gmail.com>
2013-11-12 21:58:38 -0200

    Update PedidoTest.java

--- src/main/java/com/williansmartins/teste/PedidoTest.java
+++ src/main/java/com/williansmartins/teste/PedidoTest.java
@@ -11,11 +11,12 @@ public class PedidoTest {
 
 	@Test
 	public void test() {
-		Integer id = new PedidoDAOJDBC().save("willians", "pao", "suco", null);
-		Pedido pedido = new PedidoDAOJDBC().findById(id);
+		Pedido pedidoMockado = new Pedido(null, "willians", "pao", "suco");
+		Integer id_mockado = new PedidoDAOJDBC().save(pedidoMockado);
+		Pedido pedidoBuscado = new PedidoDAOJDBC().findById(id_mockado);
 		
-		Assert.assertEquals(id, pedido.getId());
-		Assert.assertEquals(pedido.getNome(), "willians");
+		Assert.assertEquals(id_mockado, pedidoBuscado.getId());
+		Assert.assertEquals(pedidoBuscado.getNome(), pedidoMockado.getNome());
 		
 	}
 }
