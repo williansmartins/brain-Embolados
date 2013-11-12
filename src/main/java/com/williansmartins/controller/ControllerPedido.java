84d441a013ca132a0ead9b13ceeadcabf59fea0e
williansmartins <professorwillians@gmail.com>
2013-11-12 21:53:50 -0200

    Update ControllerPedido.java

--- src/main/java/com/williansmartins/controller/ControllerPedido.java
+++ src/main/java/com/williansmartins/controller/ControllerPedido.java
@@ -12,45 +12,55 @@ import com.williansmartins.model.Pedido;
 @SessionScoped
 public class ControllerPedido {
 	
-	private String nome;
-	private String lanche;
-	private String bebida;
+	private Pedido pedido;
 	
 	public ControllerPedido(){
-		
+		pedido = new Pedido();
+	}
+	
+	public String home(){
+		pedido = new Pedido();
+		return "index.xhtml?faces-redirect=true";
+	}
+	
+	public String list(){
+		pedido = new Pedido();
+		return "lista.xhtml?faces-redirect=true";
 	}
 	
 	public String save(){
-		new PedidoDAOJDBC().save(nome, lanche, bebida, null);
+		new PedidoDAOJDBC().save(pedido);
+		pedido = new Pedido();
 		return "lista.xhtml";
 	}
 	
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getLanche() {
-		return lanche;
-	}
-
-	public void setLanche(String lanche) {
-		this.lanche = lanche;
-	}
-
-	public String getBebida() {
-		return bebida;
-	}
-
-	public void setBebida(String bebida) {
-		this.bebida = bebida;
-	}
-
+	public String remove(){
+		new PedidoDAOJDBC().remove(pedido.getId());
+		return "lista.xhtml";
+	}	
+	
+	public String incAlt(){
+		pedido = new PedidoDAOJDBC().findById(pedido.getId());
+		return "inserir.xhtml";
+	}	
+	
+	public String prepareInsert(){
+		pedido = new Pedido();
+		System.out.println("insert");
+		return "inserir.xhtml?faces-redirect=true";
+	}	
+	
 	public List<Pedido> getPedidoList() {
 		return new PedidoDAOJDBC().findAll();
 	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	
 	
 }
