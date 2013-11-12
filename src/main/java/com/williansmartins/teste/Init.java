18d40889851ebf73a437dfb976fb9822b189a71e
williansmartins <professorwillians@gmail.com>
2013-11-12 21:55:24 -0200

    Update Init.java

--- src/main/java/com/williansmartins/teste/Init.java
+++ src/main/java/com/williansmartins/teste/Init.java
@@ -2,9 +2,6 @@ package com.williansmartins.teste;
 
 import com.williansmartins.dao.PedidoDAOJDBC;
 
-//import com.williansmartins.dao.DAOFactory;
-//import com.williansmartins.dao.PedidoDAO;
-
 public class Init {
 	public static void main(String[] args) {
 		new PedidoDAOJDBC().init();
